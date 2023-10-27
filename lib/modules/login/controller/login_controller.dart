2b19598405344f18fca3c37baf876ca00074b7bf
kimwest00 <minseohufs@hufs.ac.kr>
2023-10-27 13:29:46 +0900

    fix: layout 수정 + kakao login api 코드 작성

--- lib/modules/login/controller/login_controller.dart
+++ lib/modules/login/controller/login_controller.dart
@@ -1,56 +1,42 @@
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 
-class LoginController extends GetxController{
-  // ///*  카카오 로그인
-  // Future<void> signInWithKakao() async {
-  //   bool install = await isKakaoTalkInstalled();
-  //
-  //   if (install) {
-  //     try {
-  //       await UserApi.instance.loginWithKakaoTalk();
-  //
-  //       User user = await UserApi.instance.me();
-  //
-  //       return {
-  //         'email': user.kakaoAccount?.email,
-  //         'provider': 'kakao',
-  //         'providerId': user.id.toString()
-  //       };
-  //     } catch (error) {
-  //       if (error is PlatformException && error.code == 'CANCELED') {
-  //         return null;
-  //       }
-  //
-  //       // 카카오톡에 연결된 카카오계정이 없는 경우, 카카오계정으로 로그인
-  //       try {
-  //         var code = await UserApi.instance.loginWithKakaoAccount();
-  //         User user = await UserApi.instance.me();
-  //         return {
-  //           'email': user.kakaoAccount?.email,
-  //           'provider': 'kakao',
-  //           'providerId': user.id.toString()
-  //         };
-  //       } catch (error2) {
-  //         // Logger().d('카카오계정으로 로그인 실패 $error2');
-  //         return null;
-  //       }
-  //     }
-  //   } else {
-  //     try {
-  //       var e = await UserApi.instance.loginWithKakaoAccount();
-  //       User user = await UserApi.instance.me();
-  //
-  //       return {
-  //         'email': user.kakaoAccount?.email,
-  //         'provider': 'kakao',
-  //         'providerId': user.id.toString()
-  //       };
-  //     } catch (error) {
-  //       // Logger().d('카카오계정으로 로그인 실패 $error');
-  //       return null;
-  //     }
-  //   }
-  // }
+class LoginController extends GetxController {
+  ///*  카카오 로그인
+  Future<void> signInWithKakao() async {
+    // 카카오 설치여부 확인
+    bool install = await isKakaoTalkInstalled();
+    if (install) {
+      try {
+        // 카카오 로그인 내부 메소드 호출
+        await UserApi.instance.loginWithKakaoTalk();
+        User user = await UserApi.instance.me();
+      } catch (error) {
+        if (error is PlatformException && error.code == 'CANCELED') {
+          return null;
+        }
+
+        try {
+          var code = await UserApi.instance.loginWithKakaoAccount();
+          User user = await UserApi.instance.me();
+
+        } catch (error2) {
+          // Logger().d('카카오계정으로 로그인 실패 $error2');
+          return null;
+        }
+      }
+    } else {
+      try {
+        var e = await UserApi.instance.loginWithKakaoAccount();
+        User user = await UserApi.instance.me();
+      } catch (error) {
+        // Logger().d('카카오계정으로 로그인 실패 $error');
+        return null;
+      }
+    }
+  }
+
   @override
   void onInit() {
     // TODO: implement onInit
--- lib/modules/login/view/login_view.dart
+++ lib/modules/login/view/login_view.dart
@@ -3,6 +3,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:get/get.dart';
 import 'package:ibaji/modules/login/controller/login_controller.dart';
+import 'package:ibaji/util/app_text_styles.dart';
 
 import '../../../util/app_colors.dart';
 import '../../../util/global_variables.dart';
@@ -13,36 +14,28 @@ class LoginScreen extends GetView<LoginController> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: AppColors.primary6,
         body: Column(
-      mainAxisAlignment: MainAxisAlignment.spaceAround,
-      children: [
-        Image.asset("${imgDir}logo/ic_home_logo.png"),
-        GestureDetector(
-          onTap: () {
-
-          },
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 53.w),
-              decoration: BoxDecoration(
-                  color: AppColors.kakao,
-                  borderRadius: BorderRadius.circular(10.r)),
-              padding: EdgeInsets.symmetric(vertical: 13.h, horizontal: 19.w),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset("${imgDir}logo/ic_login_kakao_18.svg"),
-                  SizedBox(
-                    width: 20.w,
-                  ),
-                  Text('카카오로 로그인하기'),
-                ],
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Expanded(child: Center(child: SvgPicture.asset("${imgDir}logo/ic_home_logo.svg"))),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                padding: EdgeInsets.symmetric(vertical: 14.h, horizontal: 16.w),
+                margin: EdgeInsets.symmetric(horizontal: 24.w).copyWith(bottom: 90.h),
+                decoration: BoxDecoration(
+                    color: AppColors.kakao,
+                    borderRadius: BorderRadius.circular(10.r)),
+                child: Row(
+                  children: [
+                    SvgPicture.asset("${imgDir}logo/ic_login_kakao_18.svg"),
+                    Expanded(child: Center(child: Text('카카오로 로그인하기',style: AppTextStyles.title2Bold,))),
+                  ],
+                ),
               ),
-            ),
-          ),
-        )
-      ],
-    ));
+            )
+          ],
+        ));
   }
 }

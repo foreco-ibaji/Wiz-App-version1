ee3e3a5d948e13c3e0e46422708c4f5215d99f01
seongjunkim <tjdwns5063@naver.com>
2024-02-02 16:01:56 +0900

    feat: 마이페이지 UI 컴포넌트 구현

---
+++ lib/modules/mypage/widget/use_info.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:ibaji/modules/mypage/widget/mypage_button.dart';
+import 'package:ibaji/util/app_text_styles.dart';
+import 'package:ibaji/util/global_variables.dart';
+
+class UseInfo extends StatelessWidget {
+  const UseInfo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          "이용 안내",
+          style: AppTextStyles.title1Bold,
+        ),
+        SizedBox(
+          height: 16.h,
+        ),
+        MyPageButton(
+            onPressed: () {}, iconUrl: "${iconDir}ic_mail.svg", label: "문의하기"),
+        MyPageButton(
+            onPressed: () {},
+            iconUrl: "${iconDir}ic_clipboard.svg",
+            label: "약관 및 정책"),
+        MyPageButton(
+            onPressed: () {},
+            iconUrl: "${iconDir}ic_settings.svg",
+            label: "설정"),
+      ],
+    );
+  }
+}
---
+++ lib/modules/mypage/widget/user_info_card.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:ibaji/provider/service/map_service.dart';
+import 'package:ibaji/util/app_colors.dart';
+import 'package:ibaji/util/app_text_styles.dart';
+import 'package:ibaji/util/extension.dart';
+import 'package:ibaji/util/global_variables.dart';
+
+class UserInfoCard extends StatelessWidget {
+  const UserInfoCard(
+      {super.key,
+      required this.profileUrl,
+      required this.name,
+      required this.point});
+
+  final String profileUrl;
+  final String name;
+  final int point;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+        padding: EdgeInsets.symmetric(horizontal: 16.w, vertical: 20.h),
+        decoration: BoxDecoration(
+          color: AppColors.primary5,
+          borderRadius: BorderRadius.circular(8.r),
+        ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                CircleAvatar(
+                  radius: 30.r,
+                  backgroundImage: NetworkImage(profileUrl),
+                ),
+                SizedBox(
+                  width: 20.w,
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      name,
+                      style: AppTextStyles.heading3Bold
+                          .copyWith(color: AppColors.grey1),
+                    ),
+                    Text(
+                        "서울 ${MapService.currentAddress[1]} ${MapService.currentAddress[2]}",
+                        style: AppTextStyles.title3Medium
+                            .copyWith(color: AppColors.grey1))
+                  ],
+                ),
+              ],
+            ),
+            SizedBox(
+              height: 18.h,
+            ),
+            Divider(
+              thickness: 1.h,
+              color: AppColors.primary6,
+            ),
+            SizedBox(
+              height: 10.h,
+            ),
+            Text("위즈 머니",
+                style:
+                    AppTextStyles.title2Bold.copyWith(color: AppColors.grey1)),
+            Row(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Text("$point".transformMoneyFormat(),
+                      style: AppTextStyles.heading2Bold
+                          .copyWith(color: AppColors.grey1)),
+                  SizedBox(
+                    width: 6.w,
+                  ),
+                  SvgPicture.asset("${iconDir}ic_mission_money.svg")
+                ]),
+          ],
+        ));
+  }
+}
